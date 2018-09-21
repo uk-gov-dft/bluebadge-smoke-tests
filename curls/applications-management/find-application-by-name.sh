7982f34b3018305c9f07cee02763d116c0c2f030
Andrew Rea <email@andrewrea.co.uk>
2018-09-21 12:24:07 +0000

    Changing to pre prod by default

--- curls/applications-management/find-application-by-name.sh
+++ curls/applications-management/find-application-by-name.sh
@@ -3,10 +3,9 @@
 ACCESS_TOKEN=$(get_access_token)
 
 schmokin "$BB_URL/applications?postCode2=SK68GH" \
-    --debug \
     --status --eq 200 \
-    --jq '.data | length'
-    --eq 1
+    --jq '.data | length' \
+    --ge 1 \
     -- \
     -X GET \
     -H "accept: application/json" \
--- smoke_test.sh
+++ smoke_test.sh
@@ -2,23 +2,6 @@
 
 set -e
 
-#   echo
-#   echo "Setup"
-#   printf '%0.1s' "-"{1..55}
-#   printf '\n'
-#   # Reset the database and truncate all the tables from bb_dev
-#   bash db/reset-db.sh
-#   printf "%50s ${GREEN}%s${NC}\n" "Database reset" "DONE"
-
-#   # Add the initial required data
-#   bash db/setup_data.sql > /dev/null
-#   printf "%50s ${GREEN}%s${NC}\n" "Database refreshed" "DONE"
-
-echo "Health Check"
-printf '%0.1s' "-"{1..55}
-printf '\n'
-bash curls/health.sh
-
 # run the badge management smoke tests
 echo 
 echo "Badge Management"
