301485cf1b57408bc3174586bb527ba3409b21af
Andrew Rea <email@andrewrea.co.uk>
2018-10-08 09:08:35 +0000

    Change environments

--- smoke_test.sh
+++ smoke_test.sh
@@ -14,12 +14,12 @@ bash curls/badge-management/replace-a-badge.sh
 bash curls/badge-management/cancel-a-badge.sh
 bash curls/badge-management/delete-a-badge.sh
 
-echo 
-echo "User Management"
-echo
-bash curls/user-management/create-a-user.sh
-bash curls/user-management/create-a-user-fails.sh
-bash curls/user-management/find-users-by-la-id.sh
+# echo 
+# echo "User Management"
+# echo
+# bash curls/user-management/create-a-user.sh
+# bash curls/user-management/create-a-user-fails.sh
+# bash curls/user-management/find-users-by-la-id.sh
 
 
 echo
