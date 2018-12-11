29c938bbd44bc2b3a790afa2296e4e06eb0e8e94
Andy Rea <andrew.rea@andrewreaassociates.com>
2018-12-11 08:37:39 +0000

    Updating the smoke tests with the correct enum values

--- curls/badge-management/get-a-badge.sh
+++ curls/badge-management/get-a-badge.sh
@@ -2,7 +2,7 @@
 
 ACCESS_TOKEN=$(get_access_token)
 
-schmokin '$BB_URL/badges/$badgeNumber' --jq '.data | .statusCode' --eq 'ISSUED' --\
+schmokin '$BB_URL/badges/$badgeNumber' --jq '.data | .statusCode' --eq 'ORDERED' --\
     -H "accept: application/json" \
     -H "Content-Type: application/json" \
     -H "Authorization: Bearer $ACCESS_TOKEN"
